8b7f42c6943673cc66da737aabaf3060694b987f
Bobby Johnson <Bobby@MadnessLabs.net>
2016-02-09 16:37:07 -0600

    Add guess input

--- www/js/build.js
+++ www/js/build.js
@@ -69084,61 +69084,6 @@ var MadnessValentine;
 })(MadnessValentine || (MadnessValentine = {}));
 
 /// <reference path='../../tsd/tsd.d.ts'/>
-var MadnessValentine;
-(function (MadnessValentine) {
-    var DestinationFactory = (function () {
-        function DestinationFactory($http, $q, $httpParamSerializer, $rootScope) {
-            this.$http = $http;
-            this.$q = $q;
-            this.$httpParamSerializer = $httpParamSerializer;
-            this.$rootScope = $rootScope;
-            // On Load
-            this.destinations = [
-                {
-                    name: 'Love is in Bloom',
-                    hint: 'Whether it\'s Valentine\'s or our wedding, this flowershop is unForgettable',
-                    lat: 38.347558,
-                    lng: -90.983403
-                }, {
-                    name: 'Love at First Bite',
-                    hint: 'First date and I knew that I could come back here with you forever',
-                    lat: 38.558107,
-                    lng: -91.012088
-                }, {
-                    name: 'Love is Straightforward',
-                    hint: 'I have never had a more amazing first date. Owl City, snow, and a girl curled in my lap',
-                    lat: 38.590108,
-                    lng: -90.976528
-                }, {
-                    name: 'Love Buds',
-                    hint: 'Though it was a stressful time, we had fun and learned lessons',
-                    lat: 38.433605,
-                    lng: -90.956886
-                }, {
-                    name: 'Love your Friends',
-                    hint: 'Former love drunk, current drunk, stand up guy, and forever a friend',
-                    lat: 38.351658,
-                    lng: -90.976509
-                }, {
-                    name: 'Love will find you Home',
-                    hint: 'Every quest must come to the end, but love lives forever',
-                    lat: 38.187028,
-                    lng: -90.836334
-                }
-            ];
-        }
-        DestinationFactory.prototype.all = function () {
-            return this.destinations;
-        };
-        DestinationFactory.prototype.get = function (index) {
-            return this.destinations[index];
-        };
-        return DestinationFactory;
-    })();
-    angular.module('MadnessValentine').service('destinations', DestinationFactory);
-})(MadnessValentine || (MadnessValentine = {}));
-
-/// <reference path='../../tsd/tsd.d.ts'/>
 angular.module('MadnessValentine').directive('noScroll', function ($document) {
     return {
         restrict: 'A',
@@ -69162,35 +69107,11 @@ var MadnessValentine;
             // On Load
             this.destinations = [
                 {
-                    name: 'Love is in Bloom',
-                    hint: 'Whether it\'s Valentine\'s or our wedding, this flowershop is unForgettable',
-                    lat: 38.347558,
-                    lng: -90.983403
-                }, {
-                    name: 'Love at First Bite',
-                    hint: 'First date and I knew that I could come back here with you forever',
-                    lat: 38.558107,
-                    lng: -91.012088
-                }, {
-                    name: 'Love is Straightforward',
-                    hint: 'I have never had a more amazing first date. Owl City, snow, and a girl curled in my lap',
-                    lat: 38.590108,
-                    lng: -90.976528
-                }, {
-                    name: 'Love Buds',
-                    hint: 'Though it was a stressful time, we had fun and learned lessons',
-                    lat: 38.433605,
-                    lng: -90.956886
-                }, {
-                    name: 'Love your Friends',
-                    hint: 'Former love drunk, current drunk, stand up guy, and forever a friend',
-                    lat: 38.351658,
-                    lng: -90.976509
-                }, {
-                    name: 'Love will find you Home',
-                    hint: 'Every quest must come to the end, but love lives forever',
-                    lat: 38.187028,
-                    lng: -90.836334
+                    name: 'Luscious Blossoms',
+                    hint: 'Though it\'s not the typical arrangement it says I love you berry much.',
+                    answer: 'Edible Arrangements',
+                    lat: 38.437956,
+                    lng: -90.382641
                 }
             ];
         }
